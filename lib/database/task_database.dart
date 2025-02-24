af5639929bcbf7d5cff7ed7a0b537f1a0835218b
JonathanRomeraso <21030021@itcelaya.edu.mx>
2025-02-23 23:42:01 -0600

    21 Febrero

--- lib/database/task_database.dart
+++ lib/database/task_database.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter_application_1/models/todo_model.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -14,19 +15,47 @@ class TaskDatabase {
     if (_database != null) return _database!;
     return _database = await initDatabase();
   }
-  
+
   Future<Database?> initDatabase() async {
     Directory directory = await getApplicationDocumentsDirectory();
-    
+
     String path = join(directory.path, NAMEDB);
     return openDatabase(
       path,
       version: VERSIONDB,
       onCreate: (db, version) {
-        String query = 'CREATE TABLE .....';
+        String query = '''CREATE TABLE todo ( 
+          idTodo integer primary key,
+          titleTodo varchar(35),
+          dscTodo varchar(100),
+          dateTodo varchar(10),
+          sttTodo boolean,
+        );''';
         db.execute(query);
       },
-  );
-    
+    );
+  }
+
+  Future<int> INSERT(String table, Map<String, dynamic> map) async {
+    final con = await database;
+    return con!.insert(table, map);
+  }
+
+  Future<int> UPDATE(String table, Map<String, dynamic> map) async {
+    final con = await database;
+    return con!
+        .update(table, map, where: 'idTodo = ?', whereArgs: [map['idTodo']]);
+  }
+
+  Future<int> DELETE(String table, int idTodo) async {
+    final con = await database;
+    return con!.delete(table, where: 'idTodo = ?', whereArgs: [idTodo]);
+  }
+
+  Future<List<TodoModel>> SELECT() async {
+        final con = await database;
+        var result = await con!.query('todo');
+        return result.map((task) => TodoModel.fromMap(task)).toList();
+
   }
 }
